689287356ee63105694812870954cf18d6d5bbc2
AsianDave94 <Davidtjmalmberg@gmail.com>
2017-05-03 12:26:24 +0200

    framework

---
+++ MVVMStarterV31/MVVMStarter/Models/Domain/Photo/DomainModel.cs
@@ -0,0 +1,35 @@
+﻿using MVVMStarter.Models.Base;
+using MVVMStarter.Persistency.Base;
+
+namespace MVVMStarter.Models.Domain.Photo
+{
+    /// <summary>
+    /// Model class for the example Photo class.
+    /// The implementation is generic, and is made 
+    /// domain-specific by the namespace only.
+    /// </summary>
+    public class DomainModel : DomainModelBase<Photo>
+    {
+        #region Model Singleton implementation
+        private static DomainModel _instance = null;
+
+        public static DomainModel Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                _instance = new DomainModel();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Use a file-based persistent source
+        /// </summary>
+        private DomainModel() : base(new CollectionBase<Photo>(), new FileSourceBase<Photo>())
+        {
+        }
+        #endregion
+    }
+}
+
