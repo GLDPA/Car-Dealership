8847de8c4f99150e36732cc60a5e97bc19bf48c8
AsianDave94 <Davidtjmalmberg@gmail.com>
2017-05-07 11:08:48 +0200

    SP

---
+++ MVVMStarterV32A/MVVMStarter/Models/Domain/SP/SPClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMStarter.Models.Domain.Student;
+
+namespace MVVMStarter.Models.Domain.SP
+{
+    class SPClass : Person
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username;}
+            set { value = _username;}
+        }
+
+        public string Password
+        {
+            get { return _password;}
+            set { value = _password;}
+        }
+    }
+}
